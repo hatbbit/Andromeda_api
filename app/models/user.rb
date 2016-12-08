7072f234c1c33cceb55fed170ff8ad6db867a931
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-12-08 17:10:35 +0900

    Revert "Remove validate for email_confirmation"
    This reverts commit 7d9337d610d9f0c65053367a86ed61a1b2886e1a.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :stocks
 
   validates :email, confirmation: true
+  validates :email_confirmation, presence: true
 
   validates :name, length: { maximum: 255 }, presence: true
   validates :email, length: { maximum: 255 }, presence: true
