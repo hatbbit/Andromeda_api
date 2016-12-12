06094f60c01da65114ca4f02be2183121bf18e65
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-12-12 18:59:17 +0900

    Add test of called model method

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,12 +10,22 @@ RSpec.describe UsersController, type: :controller do
       get :index
       expect(response).to have_http_status(200)
     end
+
+    it "call User.all once" do
+      expect(User).to receive(:all).once
+      get :index
+    end
   end
 
   describe "GET #show" do
     it "response successfully with an hTTP 200 status code" do
       get :show, params: { id: user.id }
       expect(response).to have_http_status(200)
+    end
+
+    it "call User.find with GET user.id paramerter once" do
+      expect(User).to receive(:find).with(user.id.to_s).once
+      get :show, params: { id: user.id }
     end
   end
 
@@ -24,12 +34,22 @@ RSpec.describe UsersController, type: :controller do
       post :create, params: { user: { name: user.name, email: user.email, email_confirmation: user.email, password: user.password } }
       expect(response).to have_http_status(201)
     end
+
+    it "call save with GET user params once" do
+      expect_any_instance_of(User).to receive(:save).once
+      post :create, params: { user: { name: user.name, email: user.email, email_confirmation: user.email, password: user.password } }
+    end
   end
 
   describe "PATCH #update" do
     it "response successfully with an hTTP 200 status code" do
-      patch :update, params: { id: user.id, user: { name: "Taro" } }
+      patch :update, params: { id: user.id, user: { name: user.name } }
       expect(response).to have_http_status(200)
+    end
+
+    it "call update with GET user params once" do
+      expect_any_instance_of(User).to receive(:update).once
+      patch :update, params: { id: user.id, user: { name: user.name } }
     end
   end
 
@@ -37,6 +57,11 @@ RSpec.describe UsersController, type: :controller do
     it "response successfully with an hTTP 204 status code" do
       delete :destroy, params: { id: user.id }
       expect(response).to have_http_status(204)
+    end
+
+    it "call delete with GET user params once" do
+      expect_any_instance_of(User).to receive(:destroy).once
+      delete :destroy, params: { id: user.id }
     end
   end
 end
